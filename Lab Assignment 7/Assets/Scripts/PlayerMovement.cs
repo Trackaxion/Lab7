708af768562a6a6d616d23554f8b41aea4f1b98a
Donovan Peckham <donovanapeckham@hotmail.com>
2025-02-21 17:40:57 -0500

    Added Gravity

--- Lab Assignment 7/Assets/Scripts/PlayerMovement.cs
+++ Lab Assignment 7/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController characterController;
     private Vector3 playerDirection;
     private Camera mainCamera;
-
+    private float gravity = -9.81f;
+    private float velocity;
+    [SerializeField] private float gravityMultiplier = 3f;
     [SerializeField] private float rotationSpeed = 500f;
     [SerializeField] private float speed;
 
@@ -22,7 +24,22 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         ApplyRotation();
+        ApplyGravity();
         ApplyMovement();
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded && velocity < 0f)
+        {
+            velocity = -1f;
+        }
+        else
+        {
+            velocity += gravity * gravityMultiplier * Time.deltaTime;
+        }
+       
+        playerDirection.y = velocity;
     }
 
     private void ApplyRotation()
